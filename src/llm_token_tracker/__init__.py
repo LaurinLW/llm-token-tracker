c4f4e6cc886ac722e9a704bc311c2fb8099cde37
Laurin Wittich <laurin.leon.w@gmail.com>
2025-10-15 11:29:12 +0200

    feat: verbosity

--- src/llm_token_tracker/__init__.py
+++ src/llm_token_tracker/__init__.py
@@ -1,5 +1,7 @@
 from .wrapper import TokenTracker
+from typing import Literal
 
-def wrap_llm(llm):
-    TokenTracker(llm)
+
+def wrap_llm(llm, verbosity: Literal["minimum", "detailed", "max"] = "minimum"):
+    TokenTracker(llm, verbosity)
     return llm
